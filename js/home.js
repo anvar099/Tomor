9b7d45aef10212200d6d1a12d46be10e03188bb4
Anvar <63100534+anvar099@users.noreply.github.com>
2022-04-19 13:05:06 +0500

    homepage

---
+++ js/home.js
@@ -0,0 +1,46 @@
+$(".new-products__row").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  infinite: true,
+  slidesToScroll: 1,
+  nextArrow: ".next__card",
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //   },
+    // },
+  ],
+});
+
+
+// $(".new-products__row").on(
+//   "afterChange",
+//   function (event, slick, currentSlide) {
+//     if (currentSlide == count) {
+//       $(".new-products__row").slick("slickPause");
+//       setTimeout(function () {
+//         $(".new-products__row").slick("slickGoTo", 0);
+//         // $(".new-products__row").slick("slickPlay");
+//       }, autoplaySpeed);
+//     }
+//   }
+// );
