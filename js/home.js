c216fc21bd4758233f34f7617550d5d190304a18
Anvar <63100534+anvar099@users.noreply.github.com>
2022-04-20 03:38:45 +0500

    home page carousels is not working

--- js/home.js
+++ js/home.js
@@ -25,15 +25,15 @@ $(".new-products__row").slick({
 });
 
 
-$(".spring__slider").slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 2,
-  infinite: true,
-  slidesToScroll: 1,
-  nextArrow: ".spring__next-card",
-  prevArrow: ".spring__prev-card",
+// $(".spring__slider").slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 2,
+//   infinite: true,
+//   slidesToScroll: 1,
+//   nextArrow: ".spring__next-card",
+//   prevArrow: ".spring__prev-card",
   // responsive: [
   //   {
   //     breakpoint: 992,
@@ -50,18 +50,62 @@ $(".spring__slider").slick({
   //     },
   //   },
   // ],
+// });
+
+
+$(".designers__row").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  infinite: true,
+  arrows: true,
+  nextArrow: ".favourite__next-arrow",
+  prevArrow: ".favourite__prev-arrow",
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 
-// $(".new-products__row").on(
-//   "afterChange",
-//   function (event, slick, currentSlide) {
-//     if (currentSlide == count) {
-//       $(".new-products__row").slick("slickPause");
-//       setTimeout(function () {
-//         $(".new-products__row").slick("slickGoTo", 0);
-//         // $(".new-products__row").slick("slickPlay");
-//       }, autoplaySpeed);
-//     }
-//   }
-// );
+$(".classic__row").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  infinite: true,
+  arrows: true,
+  // nextArrow: ".favourite__next-arrow",
+  // prevArrow: ".favourite__prev-arrow",
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
