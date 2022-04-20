4fa8de25ad68ef2f8b06781c1e5cf8340de7597c
Anvar <63100534+anvar099@users.noreply.github.com>
2022-04-20 17:58:13 +0500

    home page

--- js/home.js
+++ js/home.js
@@ -24,34 +24,33 @@ $(".new-products__row").slick({
   ],
 });
 
-
-// $(".spring__slider").slick({
-//   dots: false,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 2,
-//   infinite: true,
-//   slidesToScroll: 1,
-//   nextArrow: ".spring__next-card",
-//   prevArrow: ".spring__prev-card",
-  // responsive: [
-  //   {
-  //     breakpoint: 992,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 1,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 768,
-  //     settings: {
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //     },
-  //   },
-  // ],
-// });
-
+$(".spring__slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  rows: 2,
+  infinite: true,
+  slidesToScroll: 1,
+  nextArrow: ".spring__next-card",
+  prevArrow: ".spring__prev-card",
+responsive: [
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+],
+});
 
 $(".designers__row").slick({
   dots: false,
@@ -81,31 +80,27 @@ $(".designers__row").slick({
   ],
 });
 
-
-$(".classic__row").slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 2,
-  infinite: true,
-  arrows: true,
-  // nextArrow: ".favourite__next-arrow",
-  // prevArrow: ".favourite__prev-arrow",
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
+var swiperClassic = new Swiper(".swiperClassic", {
+  loop: true,
+  navigation: {
+    nextEl: ".classic__next-arr ",
+    prevEl: ".classic__prev-arr ",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: "auto",
     },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
+  },
 });
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   grid: {
+//     rows: 2,
+//   },
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
