43607fc935f9cc091d5095db43e5ca0a206d22be
Anvar <63100534+anvar099@users.noreply.github.com>
2022-04-24 02:11:45 +0500

    pdp done

--- js/pdp.js
+++ js/pdp.js
@@ -52,3 +52,26 @@ $(".s__products-sagestion").slick({
     },
   ],
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  direction: "vertical",
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
